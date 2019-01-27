d88e3dff0a31368729047c88d3c8799eeb188a5f
MSI\Kilon <d0414337@fcu.edu.tw>
2019-01-27 17:14:22 +0800

    V0.2

--- Planner.js
+++ Planner.js
@@ -1,8 +1,14 @@
 // JavaScript Document
 var LocationX=0;
 var LocationY=0;
-var LocatingObject=null;
+var MouseLocationX=0;
+var MouseLocationY=0;
+var SelectingObject=null;
 var VisualSpace=Array();
+var Objects = Array();
+Create_Moving_Object();
+Create_Object_Following_Mouse();
+document.body.addEventListener("mouseup",Clear_SelectingObject);
 
 function Object(ID,Name,Color,Width,Height,Price)
 {
@@ -17,25 +23,7 @@ function Object(ID,Name,Color,Width,Height,Price)
 //var Objects = ReadData();
 //document.getElementById("Emulator").innerHTML = Objects[1].Name;
 /*
-//insert Color for ,Geting Data from FromDataBase.txt
-function ReadData()
-{
-	var Objects = Array();
-	Objects[0] = new Object(0,'0','0','0','0','0');
-	Objects[1] = new Object(1,'1','1','1','1','1');
-	return Objects;
-}
 
-//function used by Item_Bar if mousedown add Object Data to Object_Follow_Mouse()
-function Create_Moving_Object()
-{
-	
-}
-//create a div to save Data Selected,if mouseup add Object ID & Location To AddObjectToPlannerEmulator()
-function Object_Follow_Mouse()
-{
-	var Object = {ID:1,Name:"1",Color:"1",Width:"1",Height:"1",Price:"1"};
-}
 //create data at location,place pic & Calculate Using Space
 function AddObjectToPlannerEmulator()
 {
@@ -47,7 +35,7 @@ function Calculate_Using_Place()
 	
 }
 */
-//create a Simulation Planner Place,5CM = 5px.
+//create a Simulation Planner Place,1CM = 1px.
 function Planner_Emulator(object)
 {
 	//document.getElementById("Item_Bar").innerHTML = "123";
@@ -57,10 +45,9 @@ function Planner_Emulator(object)
 	var Width=document.getElementById("Width").value
 	var Height=document.getElementById("Height").value;
 	
-	document.getElementById("Item_Bar").innerHTML = Width + "<br>" + Height;
 	CreateEmtryElement(Height,Width);
 }
-
+//create empty space in Emulator
 function CreateEmtryElement(Height,Width) 
 {
 	var i=0,j=0;
@@ -72,21 +59,35 @@ function CreateEmtryElement(Height,Width)
 		str += "<tr>";
 		for(j=0;j<Width;j++)
 		{
-			str += "<td style='border:1px solid black;width:5px;height:5px;' onmouseover='mouseon(this,"+(j+1)+","+(i+1)+")' onmouseout='mouseout(this)' >" + "" + "</td>";
+			str += "<td style='border:1px solid black;width:1px;height:1px;' onmouseover='mouseon(this,"+(j+1)+","+(i+1)+")' onmouseout='mouseout(this)' >" + "" + "</td>";
 		}
 		str += "<tr>";
 	}
 	str += "</table>";
 	document.getElementById("Emulator").innerHTML = str;
 }
+//Geting Data
+function ReadData()
+{
+	Objects[0] = new Object(0,'table','0','100','100','0');
+}
+//function used by Item_Bar if mousedown add Object Data to SelectingObject
+function Create_Moving_Object()
+{
+	ReadData();
+	var str = "<img src='Object/" + Objects[0].Name + ".jpg' name='" + Objects[0].ID + "' width=50% height=25% ondragstart='return false;' onMouseDown='Object_Selecting(this)'></img>";
+	document.getElementById("Item_Bar").innerHTML = str;
+}
 
+
+//if mouse on ,select the location
 function mouseon(location,Locx,Locy)
 {
 	location.style.background = 'black';
 	LocationX = Locx;
 	LocationY = Locy;
 }
-
+//if mouse out ,cencel select
 function mouseout(location)
 {
 	location.style.background = 'white';
@@ -94,10 +95,46 @@ function mouseout(location)
 	LocationY = 0;
 }
 
+function Create_Object_Following_Mouse()
+{
+	var Position_Now = document.getElementById("Moving_Object");
+	Position_Now.style='position:absolute;pointer-events: none;float:bottom;';
+}
+//mouse location now
 function mouse_position()
 {	
 	var posX = LocationX;
 	var posY = LocationY;
 	
 	document.getElementById("Position").innerHTML = posX + "," + posY;
+}
+//create a div to Empty prepare for Object Selected
+function Object_Following_Mouse()
+{
+	var e = window.event;
+	MouseLocationX = e.clientX;
+	MouseLocationY = e.clientY;
+	if(SelectingObject == null){document.getElementById("MousePosition").innerHTML = MouseLocationX + "," + MouseLocationY;}
+	var Position_Now = document.getElementById("Moving_Object");
+	Position_Now.style.top = MouseLocationY-SelectingObject.Height + "px";
+	Position_Now.style.left = MouseLocationX + "px";
+}
+//Select Object
+function Object_Selecting(Object)
+{
+	SelectingObject = Objects[Object.name];
+	document.getElementById("MousePosition").innerHTML = SelectingObject.Name;
+	var Position_Now = document.getElementById("Moving_Object");
+	Position_Now.style.visibility = "visible";
+	Position_Now.src = "Object/" + SelectingObject.Name + ".jpg";
+	Position_Now.height = SelectingObject.Height;
+	Position_Now.width = SelectingObject.Width;
+	Object_Following_Mouse();
+}
+//clear Object in SlectingObject
+function Clear_SelectingObject()
+{
+	SelectingObject = null;
+	var Position_Now = document.getElementById("Moving_Object");
+	Position_Now.style.visibility = "hidden";
 }
